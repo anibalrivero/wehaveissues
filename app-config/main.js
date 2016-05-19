46b190f29b68db8e2780fde5c264cc8f272cdb9b
anibal <aniba.rivero@skygeo.com>
2016-05-19 10:30:59 +0200

    Autoincrement for issue id, creation date

--- app-config/main.js
+++ app-config/main.js
@@ -16,12 +16,39 @@ A.app({
         title: 'Issues',
         fields: {
           issue_id: Fields.integer("Issue Id").readOnly(),
-          assigned_to: Fields.text("Assigned to"),
+          assigned_to: Fields.fixedReference("Assigned to", "User"),
+          creation_date: Fields.date("Date").readOnly(),
         },
-        beforeCreate: function(Entity){
-          Entity.issue_id = Math.floor(Math.random() * (10 - 1 + 1) + 1);
+        beforeSave: function (Entity, Dates, Crud) {
+          if (!Entity.creation_date) {
+            Entity.creation_date = Dates.nowDate();
+          }
+          return Crud.crudFor('IssueCounter').find({}).then(function (last) {
+            if (!Entity.issue_id) {
+              Entity.issue_id = last[0].number;
+              return Crud.crudFor('IssueCounter').updateEntity({id: last[0].id, number: last[0].number + 1});
+            }
+          })
+        },
+      },
+      IssueCounter: {
+        fields: {
+          number: Fields.integer("Counter")
+        }
+      },
+      User:{
+        permissions:{
+          read: null
         }
       }
     }
-  }
+  },
+  migrations: function (Migrations) { return [
+    {
+      name: "issue-counter",
+      operation: Migrations.insert("IssueCounter", [
+        {id: "2", number: 1}
+      ])
+    }
+  ]}
 });
